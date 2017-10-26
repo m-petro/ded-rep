8654005639ae53ca76455742ce22598cbabc2cad
Marina Petropavlovskikh <32311550+m-petro@users.noreply.github.com>
2017-10-27 01:15:20 +0300

    Update string_unescape.h

--- string_unescape.h
+++ string_unescape.h
@@ -1,70 +1,291 @@
-#ifndef __STRING_UNESCAPE_H__
-#define __STRING_UNESCAPE_H__
+#include "string_unescape.h"
 
-#define UNESCAPE_HEX 0x04
-#define UNESCAPE_OCTAL 0x02
-#define UNESCAPE_SPACE 0x01
-#define UNESCAPE_SPECIAL 0x08
+/*@ requires \valid(src) && \valid(*src);
+    requires \valid(dst) && \valid(*dst);
+    requires **src == 'x' ==> \valid(*src + 1);
+    requires isxdigit(*(*src + 1)) ==> \valid(*src + 2);
+    behavior not_hex:
+       assumes **src != 'x' ||
+          (**src == 'x' && !isxdigit(*(*src + 1)));
+       assigns \nothing;
+       ensures \result == \false;
+    behavior one_digit:
+       assumes **src == 'x';
+       assumes isxdigit(*(*src + 1));
+       assumes !isxdigit(*(*src + 2));
+       assigns **dst, *dst, *src;
+       ensures *dst == \old(*dst + 1);
+       ensures *src == \old(*src + 2);
+       ensures *(\old(*dst)) == hex_to_bin(*(*\old(src) + 1));
+       ensures \result == \true;
+   behavior two_digits:
+       assumes **src == 'x';
+       assumes isxdigit(*(*src + 1));
+       assumes isxdigit(*(*src + 2));
+       assigns **dst, *dst, *src;
+       ensures *dst == \old(*dst + 1);
+       ensures *src == \old(*src + 3);
+       ensures \result == \true;
+   complete behaviors;
+   disjoint behaviors;
+*/
+ 
+static bool unescape_hex(char **src, char **dst)
+{
+	char *p = *dst, *q = *src;
+	int digit;
+	u8 num;
 
-enum {
- false = 0,
- true = 1
-};
+	if (*q++ != 'x')
+		return false;
+		//@ assert q == *src + 1;
+	num = digit = hex_to_bin(*q++); 
+		// @ for one_digit: assert 0 <= digit <= 15; 
+		// @ for two_digits: assert 0 <= digit <= 15; 
+	if (digit < 0)
+		return false;
+		//@ assert q == *src + 2;
+	digit = hex_to_bin(*q);
+	// @ for one_digit: assert digit == -1; 
+	// @ for two_digits: assert 0 <= digit <= 15; 
+	if (digit >= 0) {
+		q++;
+		//@ assert q == *src + 3;
+		// @ assert 0 <= digit <= 16;
+		num = (num << 4) | digit;
+		//@ assert num < 300;
+	}
+	*p = num;
+	*dst += 1;
+	//@ for one_digit: assert q == (*src + 2); 
+	//@ for two_digits: assert q == (*src + 3);
+	*src = q;
+	return true;
+}
 
-typedef unsigned long __kernel_ulong_t;
-typedef _Bool bool;
-typedef unsigned char u8;
-typedef __kernel_ulong_t __kernel_size_t;
-typedef __kernel_size_t size_t;
+/*@ requires \valid(src) && \valid(*src);
+    requires \valid(dst) && \valid(*dst);
+    requires isodigit(**src) ==> \valid(*src + 1);
+    requires isodigit(*(*src + 1)) ==> \valid(*src + 2);
+    requires isodigit(*(*src + 2)) ==> \valid(*src + 3);
+    behavior not_octal:
+       assumes !isodigit(**src);
+       assigns \nothing;
+       ensures \result == \false;
+    behavior one_digit:
+       assumes isodigit(**src);
+       assumes !isodigit(*(*src + 1));
+       assigns **dst, *dst, *src;
+       ensures *dst == \old(*dst + 1);
+       ensures *src == \old(*src + 1);
+       ensures *(\old(*dst)) == (**\old(src));
+       ensures \result == \true;
+   behavior two_digits:
+       assumes isodigit(**src);
+       assumes isodigit(*(*src + 1));
+       assumes !isodigit(*(*src + 2));
+       assigns **dst, *dst, *src;
+       ensures *dst == \old(*dst + 1);
+       ensures *src == \old(*src + 2);
+       ensures *(\old(*dst)) == (**\old(src)) + 8 * (*(\old(*src + 1)));
+       ensures \result == \true;
+   behavior three_digits:
+	   assumes isodigit(**src);
+       assumes isodigit(*(*src + 1));
+       assumes isodigit(*(*src + 2));
+       assigns **dst, *dst, *src;
+       ensures *dst == \old(*dst + 1);
+       ensures *src == \old(*src + 3);
+       ensures *(\old(*dst)) == (**\old(src)) + 8 * (*(\old(*src + 1))) + 64 * (*(\old(*src + 2)));
+       ensures \result == \true;
+   complete behaviors;
+   disjoint behaviors;
+*/
 
-/*@ axiomatic HexToBin {
-    logic integer hex_to_bin(integer ch);
-    axiom A0: hex_to_bin('0') == 0; 
-    axiom A1: hex_to_bin('1') == 1; 
-    axiom A2: hex_to_bin('2') == 2;
-    axiom A3: hex_to_bin('3') == 3;
-    axiom A4: hex_to_bin('4') == 4; 
-    axiom A5: hex_to_bin('5') == 5;
-    axiom A6: hex_to_bin('6') == 6; 
-    axiom A7: hex_to_bin('7') == 7;
-    axiom A8: hex_to_bin('8') == 8;
-    axiom A9: hex_to_bin('9') == 9;
-    axiom AA: hex_to_bin('a') == hex_to_bin('A') == 10;
-    axiom AB: hex_to_bin('b') == hex_to_bin('B') == 11;
-    axiom AC: hex_to_bin('c') == hex_to_bin('C') == 12;
-    axiom AD: hex_to_bin('d') == hex_to_bin('D') == 13;
-    axiom AE: hex_to_bin('e') == hex_to_bin('E') == 14;
-    axiom AF: hex_to_bin('f') == hex_to_bin('F') == 15;
+static bool unescape_octal(char **src, char **dst)
+{
+	char *p = *dst, *q = *src;
+	u8 num;
+
+	if (isodigit(*q) == 0)
+		return false;
+	num = (*q++) & 7;
+	
+	//@ assert q == *src + 1;
+	
+	/*@ loop invariant 1 <= (q - *src) <= 3;
+		loop variant (q - *src);
+	*/ 
+		
+	while (num < 32 && isodigit(*q) && (q - *src < 3)) {
+		num <<= 3;
+		num += (*q++) & 7;
+
+	}
+	
+	*p = num;
+	*dst += 1;
+	*src = q;
+	return true;
+}
+
+/*@ axiomatic Un_Space {
+    logic char unescape_space (integer ch);
+	axiom N: unescape_space ('n') == '\n';
+	axiom R: unescape_space ('r') == '\r';
+	axiom T: unescape_space ('t') == '\t';	
+	axiom V: unescape_space ('v') == '\v';
+	axiom F: unescape_space ('f') == '\f';
     }
- */
-
-/*@ predicate isxdigit(integer c) = isdigit(c)        ||
-                                    ('a' <= c <= 'f') ||
-                                    ('A' <= c <= 'F');
-*/
-/*@ predicate isdigit(integer c)  = '0' <= c <= '9';
-
 */
 
-
-/* assigns \nothing;
-    behavior to_digit:
-        assumes isxdigit(ch);
-        ensures 0 <= \result <= 15;
-        ensures \result == hex_to_bin(ch);
+/*@
+	requires \valid (src) && \valid (dst) &&
+			 \valid (*src) && \valid(*dst);
+	assigns (**dst), *src, *dst;
+    behavior is_space:
+        assumes (**src) == 'n' || (**src) == 't' ||
+				(**src) == 'r' || (**src) == 'v' ||
+				(**src) == 'f'; 
+        ensures \result == true;
+        ensures *dst == \old(*dst + 1);
+        ensures *src == \old(*src + 1);
+        ensures (**dst) == unescape_space(**src);
     behavior fail:
-        assumes !isxdigit(ch);
-        ensures \result == -1;
+        assumes (**src) != 'n' && (**src) != 't' &&
+				(**src) != 'r' && (**src) != 'v' &&
+				(**src) != 'f'; 
+        ensures \result == false;
     complete behaviors;
     disjoint behaviors;
+*/
+
+static bool unescape_space(char **src, char **dst)
+{
+	char *p = *dst, *q = *src;
+
+	switch (*q) {
+	case 'n':
+		*p = '\n';
+		break;
+	case 'r':
+		*p = '\r';
+		break;
+	case 't':
+		*p = '\t';
+		break;
+	case 'v':
+		*p = '\v';
+		break;
+	case 'f':
+		*p = '\f';
+		break;
+	default:
+		return false;
+	}
+	*dst += 1;
+	*src += 1;
+	return true;
+}
+
+/*@ axiomatic Un_Special {
+    logic char unescape_special (integer ch);
+	axiom S:  unescape_special ('\"') == '\"';
+	axiom SS: unescape_special ('\\') == '\\';
+	axiom E:  unescape_special ('e') == '\e';
+	axiom A:  unescape_special ('a') == '\a';
+    }
+*/
+
+/*@
+	requires \valid (src) && \valid (dst) &&
+			 \valid (*src) && \valid(*dst);
+	assigns (*dst), (*src);
+    behavior is_special:
+        assumes (**src) == '\"' || (**src) == '\\' ||
+				(**src) == 'a' || (**src) == 'e';
+        ensures \result == true;
+        ensures *dst == \old(*dst + 1);
+        ensures *src == \old(*src + 1);
+        ensures (**dst) == unescape_special(**src);
+    behavior fail:
+         assumes ((**src) != '\"') && ((**src) != '\\') &&
+				 ((**src) != 'a') && ((**src) != 'e');
+         ensures \result == false;
+    complete behaviors;
+    disjoint behaviors;
+*/
+
+static bool unescape_special(char **src, char **dst)
+{
+	char *p = *dst, *q = *src;
+
+	switch (*q) {
+	case '\"':
+		*p = '\"';
+		break;
+	case '\\':
+		*p = '\\';
+		break;
+	case 'a':
+		*p = '\a';
+		break;
+	case 'e':
+		*p = '\e';
+		break;
+	default:
+		return false;
+	}
+	*dst += 1;
+	*src += 1;
+	return true;
+}
+ 
+/*@ requires \valid(src + (0..size - 1));
+    requires \valid(dst + (0..size - 1));
+	behavior empty:
+	assumes 
+		(\forall integer i; 0 <= i < size ==> src[i] != '//'); 
+		\\ || (flags & UNESCAPE_SPACE == 0);  invalid operands to binary &
+	assigns dst[0..size - 1];
+	ensures \result == (size - 1);
+	ensures 
+		\forall integer i; 0 <= i < size ==> dst[i] == src[i];
+		
+	
  */
+ 
 
-extern int hex_to_bin(char ch);
+int string_unescape(char *src, char *dst, size_t size, unsigned int flags)
+{
+	char *out = dst;
 
+	while (*src && --size) {
+		if (src[0] == '\\' && src[1] != '\0' && size > 1) {
+			src++;
+			size--;
 
-//@ predicate isodigit(integer c) = '0' <= c <= '7';
-//@ ensures \result <==> isodigit(c);
+			if (flags & UNESCAPE_SPACE &&
+					unescape_space(&src, &out))
+				continue;
 
-static inline int isodigit(const char c);
+			if (flags & UNESCAPE_OCTAL &&
+					unescape_octal(&src, &out))
+				continue;
 
-#endif // __STRING_UNESCAPE_H__
+			if (flags & UNESCAPE_HEX &&
+					unescape_hex(&src, &out))
+				continue;
+
+			if (flags & UNESCAPE_SPECIAL &&
+					unescape_special(&src, &out))
+				continue;
+
+			*out++ = '\\';
+		}
+		*out++ = *src++;
+	}
+	*out = '\0';
+
+	return out - dst;
+}
