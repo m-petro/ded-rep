bd6289d8eb993c14ea79a48724039c9026e959de
Denis Efremov <efremov@ispras.ru>
2018-04-16 15:53:04 +0300

    bsearch cr

--- bsearch.c
+++ bsearch.c
@@ -3,27 +3,33 @@
 // #include <linux/bsearch.h>
 #include <string.h>
 
-/*@ 
+/* 
 	predicate sorted(char *a, int (*cmp)(char*, char*), integer n)
 									= \forall integer i, j; 0 <= i < j < n  ==> cmp(a[i], a[j]) >= 0;
 */
 
+int cmp_1(const void *key, const void *elt)
+{
+   return 0;
+}
+
 /*@
-	requires sorted((char *)base, cmp, num);
+   requires cmp == cmp_1;
 	requires \typeof(key) <: \type(char *);
 	requires \typeof(base) <: \type(char *);
 	requires \valid((char *)base +(0..(num - 1) * size));
 	requires \valid((char *)key);
 	requires \valid((char *)cmp);
+	//requires sorted((char *)base, cmp, num);
 	assigns  \nothing;
 
-	behavior exist:
-		assumes \exists integer i; 0 <= i < num  ==> cmp(((char*)base)[i], key) == 0;
-		ensures cmp(\result, key) == 0;
+	//behavior exist:
+      //assumes \exists integer i; 0 <= i < num  ==> cmp(((char*)base)[i], key) == 0;
+		//ensures cmp(\result, key) == 0;
 
-	behavior not_exist:
-		assumes !\exists integer i; 0 <= i < num ==> cmp(((char*)base)[i], key) == 0;
-		ensures \result == NULL;
+	//behavior not_exist:
+		//assumes !\exists integer i; 0 <= i < num ==> cmp(((char*)base)[i], key) == 0;
+		//ensures \result == NULL;
 */
 
 void *bsearch(const void *key, const void *base, size_t num, size_t size,
@@ -32,8 +38,8 @@ void *bsearch(const void *key, const void *base, size_t num, size_t size,
 	const char *pivot;
 	int result;
 
-	/*@
-		loop invariant base <= privot <= base + num
+	/*
+		loop invariant base <= pivot <= base + num;
 		loop variant num;
 	 */
 
